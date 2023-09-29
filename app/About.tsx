40c54003398afe860419f70c27e62046cc88995c
Les Matheson <LMATHESON4@bloomberg.net>
2023-09-29 17:08:08 -0400

    Re-ran npx create-next-app to take out the app router

--- app/About.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-
-export default function About({children}:any) {
-    return (
-        <div><h1 className="bg-red-200">Hello about page</h1> {children}</div>
-    )
-}
