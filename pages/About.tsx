364e6b7dc84cb58e3d12beb7e3f7b4f66644e088
Les Matheson <LMATHESON4@bloomberg.net>
2023-09-29 17:12:56 -0400

    Add About page

---
+++ pages/About.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export default function About({children}:any) {
+    return (
+        <div><h1 className="bg-red-200">Hello about page</h1> {children}</div>
+    )
+}
