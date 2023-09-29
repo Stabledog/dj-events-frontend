555dc96a5010ea895a67ee2eb64ae081b90af96e
Les Matheson <LMATHESON4@bloomberg.net>
2023-09-29 19:37:04 -0400

    Using router.push

--- pages/events/[slug].tsx
+++ pages/events/[slug].tsx
@@ -3,5 +3,8 @@ import { useRouter } from "next/router";
 export default function EventPage() {
     const router = useRouter()
     console.log(router)
-    return <div>slug={router.query.slug}</div>;
+    return <div>
+        slug={router.query.slug}
+        <button onClick={() => router.push('/')}>Go Home</button>
+    </div>;
 }
