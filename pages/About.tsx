51f6d3644c136ab1ad87b83ab66ffd642d940cf6
Les Matheson <LMATHESON4@bloomberg.net>
2023-09-29 17:22:11 -0400

    3:59 Pages and Routing

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 
-export default function About({children}:any) {
+export default function About() {
     return (
-        <div><h1 className="bg-red-200">Hello about page</h1> {children}</div>
+        <div><h1>Hello about page</h1> </div>
     )
 }
---
+++ pages/events/add.tsx
@@ -0,0 +1,6 @@
+
+export default function AddEventsPage() {
+  return (
+    <div>add</div>
+  )
+}
