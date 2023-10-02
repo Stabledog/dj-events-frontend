96113538915d139a1886d3b0911600f25fff64de
Les Matheson <LMATHESON4@bloomberg.net>
2023-10-02 08:53:41 -0400

    5:04 API Routes

--- pages/api/events/index.ts
+++ pages/api/events/index.ts
@@ -1,5 +1,12 @@
 const {events} = require('./data.json')
 
 export default function index(req:any,res:any) {
-    res.status(200).json(events)
+    if (req.method === 'GET') {
+        res.status(200).json(events)
+        return
+    }
+    res.setHeader('Allow',['GET'])
+    res.status(405)
+    res.json({message: `Method ${req.method} is not allowed`})
+
 }
